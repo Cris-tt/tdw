f94d23d29a15de3a0e8fadb8584a309eb031023c
Cris <cristobalterueldelacolina@gmail.com>
2026-05-09 22:03:10 -0300

    Mejore estilos y acomedé en base a corrercciones

---
+++ script.js
@@ -0,0 +1,4 @@
+var input_nombre = document.getElementById("nombre");
+var input_email = document.getElementById("email");
+var input_mensaje = document.getElementById("mensaje");
+var input_submit = document.getElementById("submit");
