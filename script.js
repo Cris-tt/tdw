cbb0928bfd1e17494613f0bd4034150fe324beba
Cris <cristobalterueldelacolina@gmail.com>
2026-05-10 17:28:57 -0300

    Cuenta regresiva y Mobile

--- script.js
+++ script.js
@@ -30,3 +30,29 @@ function enviarFormulario(event) {
     elemento_formulario = document.getElementById("formulario");
     elemento_formulario.classList.add("oculto");
 }
+
+
+
+//Funcion de contador
+function actualizarContador() {
+    const fechaActual = new Date();
+    const fechaEvento = new Date(2026, 4, 16, 12, 30, 30);
+
+    const diferencia = fechaEvento - fechaActual;  //a miiliseundos
+    // Calcular días, horas, minutos y segundos a partir de la diferencia
+    if (diferencia <= 0) {
+        document.querySelector(".contador").textContent = "¡El evento ha comenzado!";
+        clearInterval(intervalo); // Detener el intervalo si el evento ha comenzado
+        return;
+    }
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+    // Actualizar el contenido de los elementos HTML con los valores calculados
+    document.getElementById("dias").textContent = dias;
+    document.getElementById("horas").textContent = horas;
+    document.getElementById("minutos").textContent = minutos;
+    document.getElementById("segundos").textContent = segundos;
+}
+const intervalo = setInterval(actualizarContador, 1000);  //actualiza el contador cada segundo 
